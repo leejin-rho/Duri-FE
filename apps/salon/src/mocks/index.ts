10654c2b13484aeaaa412e13c98484fa2fc6d745
chanbyeol <cksquf98@naver.com>
2024-11-25 01:03:49 +0900

    feat: salon msw 적용 및 axios 설치 (DURI-309)

---
+++ apps/salon/src/mocks/index.ts
@@ -0,0 +1,10 @@
+export async function enableMocking() {
+    console.log(import.meta.env.VITE_MSW_ENABLED)
+    if (import.meta.env.VITE_MSW_ENABLED === 'true') {
+      const { worker } = await import('./browser');
+      return await worker.start();
+    }
+    else {
+      console.log('MSW is disabled');
+    }
+  }
