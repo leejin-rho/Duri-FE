b3834c9241750b278275528fb58f1a6fa37e1790
seungboshim <fodo9898@gmail.com>
2024-11-26 17:37:48 +0900

    feat: 라우팅(DURI-91)

--- apps/salon/src/App.tsx
+++ apps/salon/src/App.tsx
@@ -1,16 +1,21 @@
 // import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { globalStyle } from '@duri-fe/ui';
 import { Global } from '@emotion/react';
 
 import Home from '@pages/Home';
 
+import LoginPage from './pages/Login';
+
 function App() {
   return (
     <BrowserRouter>
       <Global styles={globalStyle} />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
