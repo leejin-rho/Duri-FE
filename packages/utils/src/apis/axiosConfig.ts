20b13eaec7049839ed6005b6646f45df4ff8b0e8
seungboshim <fodo9898@gmail.com>
2024-11-28 23:45:24 +0900

    feat: env 설정(DURI-81)

--- packages/utils/src/apis/axiosConfig.ts
+++ packages/utils/src/apis/axiosConfig.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// export const BASE_URL = import.meta.env.VITE_SERVER_API;
-// export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'https://api.example.com/api/';
+export const BASE_URL = import.meta.env.VITE_SERVER_API;
 
 export const duriInstance = axios.create({
   baseURL: BASE_URL,
