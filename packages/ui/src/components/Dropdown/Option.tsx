002f511c33f1dbaaaed79ba5236f6ab957f4d56f
chanbyeol <cksquf98@naver.com>
2024-11-28 16:42:22 +0900

    fix: hover color 수정 (DURI-314)

--- packages/ui/src/components/Dropdown/Option.tsx
+++ packages/ui/src/components/Dropdown/Option.tsx
@@ -1,13 +1,14 @@
+import { theme } from '@duri-fe/ui';
 import styled from '@emotion/styled';
 
 export const Option = styled.li`
   list-style-type: none;
-  padding: 0;
-  margin: 8px 0;
+  padding: 8px 0;
+  margin: 0;
   text-align: center;
 
   cursor: pointer;
   &:hover {
-    background-color: #f0f0f0;
+    background-color: ${theme.palette.Gray20};
   }
 `;
