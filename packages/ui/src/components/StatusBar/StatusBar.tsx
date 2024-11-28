5e14f9707d1fda5e535208d75127060656844237
seungboshim <fodo9898@gmail.com>
2024-11-28 13:45:44 +0900

    refactor: statusBar에 FlexBox 적용(DURI-90)

--- packages/ui/src/components/StatusBar/StatusBar.tsx
+++ packages/ui/src/components/StatusBar/StatusBar.tsx
@@ -1,4 +1,4 @@
-import { theme } from "@duri-fe/ui";
+import { Flex, theme } from "@duri-fe/ui";
 import styled from "@emotion/styled"
 
 interface StatusBarProps {
@@ -10,45 +10,6 @@ interface StatusBarProps {
   mode: "onboarding" | "main",
 }
 
-interface DotProps {
-  size: string,
-  color: string,
-  opacity: number,
-}
-
-const StatusBarContainer = styled.div<StatusBarProps>`
-  position: relative;
-  width: 100%;
-  height: ${({ mode }) => mode === "onboarding" ? "6px" : "3px"};
-  background-color: ${({ mode }) => mode === "onboarding" ? theme.palette.Gray100 : theme.palette.Gray50};
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const CurrentStatus = styled.div<StatusBarProps>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: ${({ current, total }) => `${(current / total) * 100}%`};
-  height: ${({ mode }) => mode === "onboarding" ? "6px" : "3px"};
-  background-color: ${theme.palette.Normal500};
-  border-radius: 10px;
-  transition: width 0.5s ease-in-out;
-  display: flex;
-  align-items: center;
-  justify-content: end;
-`;
-
-const Dot = styled.div<DotProps>`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
-  border-radius: 50%;
-  background-color: ${({ color }) => color};
-  opacity: ${({ opacity }) => opacity};
-`
-
 export const StatusBar: React.FC<StatusBarProps> = ({
   current,
   total,
@@ -56,28 +17,46 @@ export const StatusBar: React.FC<StatusBarProps> = ({
 }) => {
   const dots = Array.from({ length: total+1 }, (_, index) => {
     if (index <= current) {
-      return <Dot
-        key={index}
-        size="5px"
-        color={theme.palette.Normal500}
-        opacity={0}
-      />
+      return <HiddenDot key={index} width={5} height={5} borderRadius={5} backgroundColor={theme.palette.Gray200} />
     } else {
-      return <Dot
-        key={index}
-        size="5px"
-        color={theme.palette.Gray200}
-        opacity={1}
-      />
+      return <Dot key={index} width={5} height={5} borderRadius={5} backgroundColor={theme.palette.Gray200} />
     }
   });
 
   return (
-    <StatusBarContainer current={current} total={total} mode={mode}>
+    <StatusBarContainer
+      height={mode === "onboarding" ? "6" : "3"}
+      backgroundColor={mode === "onboarding" ? theme.palette.Gray100 : theme.palette.Gray50}
+      borderRadius={10}
+      justify="space-between"
+    >
       {mode === "main" && dots}
-      <CurrentStatus current={current} total={total} mode={mode}>
-        {mode === "main" && <Dot size="9px" color={theme.palette.Normal500} opacity={1} />}
+      <CurrentStatus
+        widthPer={(current / total) * 100}
+        height={mode === "onboarding" ? "6" : "3"}
+        backgroundColor={theme.palette.Normal500}
+        borderRadius={10}
+        justify="end"
+      >
+        {mode === "main" && <Dot width={9} height={9} borderRadius={9} color={theme.palette.Normal500} backgroundColor={theme.palette.Normal500} />}
       </CurrentStatus>
     </StatusBarContainer>
   )
 }
+
+const StatusBarContainer = styled(Flex)`
+  position: relative;
+`;
+
+const CurrentStatus = styled(Flex)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: width 0.5s ease-in-out;
+`;
+
+const HiddenDot = styled(Flex)`
+  visibility: hidden;
+`;
+
+const Dot = styled(Flex)``;
