e961b55791695b0d4cbe32fd350bd17472b2faf5
jueunseuk <junsu120202@gmail.com>
2024-11-30 16:48:25 +0900

    feat: 미용사 온보딩 빠진 스타일링 추가(DURI-91)

--- apps/salon/src/components/onboarding/Instructions.tsx
+++ apps/salon/src/components/onboarding/Instructions.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Button, MobileLayout } from '@duri-fe/ui';
+import { Button, Flex, MobileLayout, Text } from '@duri-fe/ui';
+import styled from '@emotion/styled';
 
 interface InstructionProps {
   onNext: () => void;
@@ -9,10 +10,33 @@ interface InstructionProps {
 const Instruction: React.FC<InstructionProps> = ({ onNext }) => {
   return (
     <MobileLayout>
-        <h2>두리몽실 서비스에 등록하기 위해 매장 정보가 필요합니다.</h2>
-        <Button onClick={onNext}>입력하러 가기</Button>
+      <Container
+        direction="column"
+        justify="center"
+        align="center"
+        padding="107px 20px 0 20px"
+      >
+        <Wrapper direction="column">
+          <Text typo="Heading2" align="center">
+            두리묭실 서비스에 등록하기 위해
+          </Text>
+          <Text typo="Heading2" align="center">
+            매장 정보의 등록이 필요해요
+          </Text>
+        </Wrapper>
+        <Button width="335px" height="54px" onClick={onNext}>
+          입력하러 가기
+        </Button>
+      </Container>
     </MobileLayout>
   );
 };
+
+export const Container = styled(Flex)`
+  height: 90vh;
+`;
+export const Wrapper = styled(Flex)`
+  flex-grow: 1;
+`;
 
 export default Instruction;
