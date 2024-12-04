f29da5780e01ba070374e640077450e653344d19
leejin_rho <tbdpapdl@gmail.com>
2024-12-04 10:06:20 +0900

    feat(duri): 아이콘 svg 추가(DURI-27)

--- packages/ui/src/assets/RadioUnselect.tsx
+++ packages/ui/src/assets/RadioUnselect.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-const SvgRadioUnselect = (props) => (
+const SvgRadioUnselect = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
