7ec37e9c2b4f3a8e62679d8ce285f420ab77979c
chanbyeol <cksquf98@naver.com>
2024-12-05 19:49:46 +0900

    fix: RecommendeShopProps import 경로 수정(DURI-204)

--- packages/utils/src/apis/duri/quotation.ts
+++ packages/utils/src/apis/duri/quotation.ts
@@ -1,19 +1,38 @@
-import { RequestProps } from '@duri-fe/duri/assets/types/request';
-
+// import { RequestProps } from '@duri-fe/duri/assets/types/RequestProps';
 import { duriInstance } from '../axiosConfig';
 
+interface TimeProps {
+  time9: boolean;
+  time10: boolean;
+  time11: boolean;
+  time12: boolean;
+  time13: boolean;
+  time14: boolean;
+  time15: boolean;
+  time16: boolean;
+  time17: boolean;
+  time18: boolean;
+}
+interface RequestProps extends TimeProps {
+  petId?: number;
+  menu: string[];
+  addMenu: string[];
+  specialMenu: string[];
+  design: string[];
+  etc: string;
+  day: Date;
+  shopIds: number[];
+}
 
-export const postRequestQuotation = async (
-  request: RequestProps,
-) => {
-// ): Promise<RequestResponse> => {
+export const postRequestQuotation = async (request: RequestProps) => {
+  // ): Promise<RequestResponse> => {
   const response = await duriInstance.post(`quotation/request`, request);
   return response.data;
 };
 
 export const getQuotationInfo = async (
   quotationId: string,
-// ): Promise<QuotationResponse> => {
+  // ): Promise<QuotationResponse> => {
 ) => {
   const response = await duriInstance.get(`quotation/${quotationId}`);
   return response.data;
