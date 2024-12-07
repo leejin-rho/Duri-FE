17c79ce6c034b2f85624b58fdc67a11b8d953b65
chanbyeol <cksquf98@naver.com>
2024-12-07 10:34:50 +0900

    fix: salon에서 Dropdown 타입 변경으로 인한 코드 수정, 인스턴스 수정 반영(DURI-136)

--- packages/utils/src/apis/axiosConfig.ts
+++ packages/utils/src/apis/axiosConfig.ts
@@ -3,16 +3,6 @@ import axios from 'axios';
 export const BASE_URL = import.meta.env.VITE_SERVER_API + '/api/v1';
 export const BASE_URL_AUTH = import.meta.env.VITE_SERVER_API + '/oauth2';
 
-export const publicInstance = axios.create({
-  baseURL: BASE_URL,
-  timeout: 10000,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  },
-})
-
 export const duriInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
