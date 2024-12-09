a520112d385a53e18878131034a2842904343d23
leejin_rho <tbdpapdl@gmail.com>
2024-12-10 02:15:37 +0900

    feat: 요청서 전송시 bottomSheet 구현(DURI-107)

---
+++ packages/ui/src/assets/SendColor.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+const SvgSendColor = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 56 49"
+    {...props}
+  >
+    <path fill="#707D19" d="m22.917 42.389-5.732 5.731v-8.593z" />
+    <path
+      fill="#F0FC98"
+      d="M54.995 0 36.09 48.98l-18.904-9.452v8.592l-6.874-13.748L0 27.497z"
+    />
+    <path fill="#BBD029" d="M10.386 34.372 17.26 48.12v-8.592L55.07 0z" />
+  </svg>
+);
+export default SvgSendColor;
