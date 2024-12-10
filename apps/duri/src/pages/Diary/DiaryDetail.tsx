e9957f55bea16d97226061d2d2551a7bbd0fed15
leejin_rho <tbdpapdl@gmail.com>
2024-12-10 12:55:23 +0900

    feat: 상세 페이지 펫 인포 추가(DURI-143)

--- apps/duri/src/pages/Diary/DiaryDetail.tsx
+++ apps/duri/src/pages/Diary/DiaryDetail.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 
 import {
+  Card,
   DuriNavbar,
   Flex,
   Header,
   HeightFitFlex,
   MobileLayout,
+  PetInfo,
   ProfileImage,
   RelativeFlex,
   SalonTag,
@@ -19,6 +21,9 @@ import { css } from '@emotion/react';
 const PetDiaryDetail = () => {
   const navigate = useNavigate();
 
+  const character = ['character1'];
+  const diseases = ['disease1', 'disease2'];
+
   return (
     <MobileLayout>
       <Header
@@ -26,7 +31,19 @@ const PetDiaryDetail = () => {
         backIcon={true}
         onClickBack={() => navigate(-1)}
       />
-
+      <Flex borderRadius={16} padding="0 20px" margin="0 0 16px 0">
+        <PetInfo
+          age={4}
+          breed="시츄"
+          gender="F"
+          neutering={true}
+          name="뭉뭉이"
+          weight={4.1}
+          character={character}
+          diseases={diseases}
+          image="https://s3-alpha-sig.figma.com/img/2b3d/3445/169b817c088e24ca9f6999b9f7c18e5a?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UNX-HCHQvf6OGFxdmOjpEf~gbzgcXfr7L~ZILgiSxtXRAt2cDJemS7sJOHFn177dH4-rXFgC0mu0iSo4mT02cqw0ybPZ7D-2GK5ch4XLi20GbfJjcy3yPJSXbtOonwpHQFjJDgbjRDu0VK~iz3DJSvLzAjmn5GvFaikpRDWTtJX51eL-YTGIBt7Q1vYxt66nU2dyREh1wb7u5chrtXImto2iEdFviMwJgZKP~f3K1457j~KdS~gM5gtOtm7ozWPTjdraKskNXGJhWWe9wfE74HFPFG~Tj~lY89I2fPd5TNnQI0CCghKbFOLIUyGtrJ0KceIW-gsIic-A3GWQ9IFCyg__"
+        />
+      </Flex>
       <Flex direction="column">
         <Seperator />
 
@@ -66,7 +83,6 @@ const PetDiaryDetail = () => {
         </Flex>
       </Flex>
       <Seperator />
-
       <Flex
         direction="column"
         align="start"
@@ -95,7 +111,7 @@ const PetDiaryDetail = () => {
         </Flex>
       </Flex>
       <Seperator height="5px" />
-      <RelativeFlex padding="25px 20px">
+      <RelativeFlex padding="25px 20px 117px 20px">
         <HeightFitFlex
           backgroundColor={theme.palette.Normal50}
           borderRadius={24}
