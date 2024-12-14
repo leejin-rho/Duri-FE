dbf9f1f4a2c9c514a794e73d93e7599c06b1cb43
seungboshim <fodo9898@gmail.com>
2024-12-15 02:56:06 +0900

    feat: unauthorize 로그인 던지기

--- packages/utils/src/apis/axiosConfig.ts
+++ packages/utils/src/apis/axiosConfig.ts
@@ -3,9 +3,13 @@ import axios from 'axios';
 export const BASE_URL = import.meta.env.VITE_SERVER_API + '/api/v1';
 export const BASE_URL_AUTH = import.meta.env.VITE_SERVER_API + '/oauth2';
 
+const TIME_OUT = 3000;
+const DURI_TOKEN_EMPTY = `Required request header 'authorization_user' for method parameter type String is not present`;
+const SALON_TOKEN_EMPTY = `Required request header 'authorization_shop' for method parameter type String is not present`;
+
 export const duriInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: TIME_OUT,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -15,7 +19,7 @@ export const duriInstance = axios.create({
 
 export const salonInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: TIME_OUT,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -25,7 +29,7 @@ export const salonInstance = axios.create({
 
 export const adminInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: TIME_OUT,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -35,7 +39,7 @@ export const adminInstance = axios.create({
 
 export const authInstance = axios.create({
   baseURL: BASE_URL_AUTH,
-  timeout: 10000,
+  timeout: TIME_OUT,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -45,7 +49,7 @@ export const authInstance = axios.create({
 
 export const publicInstance = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: TIME_OUT,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -64,3 +68,37 @@ salonInstance.interceptors.request.use((config) => {
   config.headers['authorization_shop'] = token ? `Bearer ${token}` : '';
   return config;
 });
+
+duriInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      if (
+        error.response.status === 401 ||
+        error.response.message === DURI_TOKEN_EMPTY
+      ) {
+        window.location.href = '/login';
+      }
+    } else {
+      console.error('Network or CORS error:', error);
+    }
+    return Promise.reject(error);
+  },
+);
+
+salonInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      if (
+        error.response.status === 401 ||
+        error.response.message === SALON_TOKEN_EMPTY
+      ) {
+        window.location.href = '/login';
+      }
+    } else {
+      console.error('Network or CORS error:', error);
+    }
+    return Promise.reject(error);
+  },
+);
