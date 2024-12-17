91b3c5cae537d57375cd87bf63ea21b8a9414f62
leejin_rho <tbdpapdl@gmail.com>
2024-12-18 04:03:45 +0900

    feat: AI 로딩 인디케이터 적용 (DURI-301)

--- apps/duri/src/pages/DooriAI/AIStyling.tsx
+++ apps/duri/src/pages/DooriAI/AIStyling.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import LoadingGIF from '@assets/images/gifs/Final_renderfile.gif';
 import BabyCut from '@assets/images/pngs/BabyCut.png';
 import BearCut from '@assets/images/pngs/BearCut.png';
 import GuideDog1 from '@assets/images/pngs/GuideDog1.png';
@@ -76,8 +77,23 @@ const AIStyling = () => {
 
   if (isPending) {
     return (
-      <MobileLayout>
-        <Text colorCode={theme.palette.White}>로딩 중</Text>
+      <MobileLayout backgroundColor={theme.palette.Black}>
+        <Flex direction="column" padding="40px 20px 0 20px">
+          <AIHeader />
+          <Flex
+            direction="column"
+            backgroundColor={theme.palette.Black}
+            gap={28}
+            padding="0 0 123px"
+          >
+            <Flex height={55}>
+              <LoadingGIFImg src={LoadingGIF} alt="loading indicator" />
+            </Flex>
+            <Text typo="Title2" colorCode={theme.palette.White}>
+              새로운 스타일로 변화 중...
+            </Text>
+          </Flex>
+        </Flex>
       </MobileLayout>
     );
   }
@@ -282,4 +298,9 @@ const SubmitButton = styled(FrontBtn)`
   bottom: 0;
   height: 53px;
   border-radius: 0;
+`;
+
+const LoadingGIFImg = styled.img`
+  width: 115px;
+  height: 66px;
 `;
